59f9040b371bab80d69ab824ee626b89150407b0
Kunal Tyagi <kunal.tyagi@crossml.com>
2026-07-14 11:35:11 +0530

    added files and folder of brand voices

---
+++ src/app/brand-voice/page.tsx
@@ -0,0 +1,13 @@
+import BrandVoice from "@/clients/brand-voice";
+
+export const metadata = {
+  title: "Brand Voice",
+};
+
+export default function Page() {
+  return (
+    <>
+      <BrandVoice />
+    </>
+  );
+}
