d7b7ee8f9ce7cac902cc85181eca33c42ac089fe
ajayverma-cml <ajay.verma@crossml.com>
2026-07-20 16:31:10 +0530

    help desk init

--- src/lib/sidebar-navs.ts
+++ src/lib/sidebar-navs.ts
@@ -5,6 +5,7 @@ import {
   IconBooks,
   IconDashboard,
   IconHelp,
+  IconHeadset,
   IconImageGeneration,
   IconMessage2,
   IconMessageUser,
@@ -61,6 +62,11 @@ export const sidebarMenus: SideBarMenus = {
       title: "Live Support",
       url: "/support",
       icon: IconMessageUser,
+    },
+    {
+      title: "Help Desk",
+      url: "/helpdesk",
+      icon: IconHeadset,
     },
     {
       title: "Knowledge",
