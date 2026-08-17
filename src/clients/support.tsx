2552809c81fcd1f80b114ae31e3f54c283459b1a
sujeet-crossml <sujeet.shah@crossml.com>
2026-08-17 16:14:42 +0530

    fix: remove unnecessary blank line in Support component

--- src/clients/support.tsx
+++ src/clients/support.tsx
@@ -1550,7 +1550,6 @@ export default function Support() {
                       }}
                     />
                   )}
-
                 </div>
               </header>
               <div className="flex min-h-0 flex-1 flex-col overflow-hidden">
